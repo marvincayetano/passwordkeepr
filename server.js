fe0674f4145193d7b5a4b10110fdf5c00fae9d33
AbdiGureye <abdifatah.gureye@gmail.com>
2021-11-29 22:51:49 +0000

    adding route for login, and requiring cookie session

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieSession = require("cookie-session")
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -18,6 +19,13 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
+
+app.use(cookieSession({
+  name: 'session',
+  secret: "mileycyrus",
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -55,3 +63,8 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+app.get("/login", (req, res) => {
+  const templateVars = {user: null}
+  res.render("login", templateVars)
+})
