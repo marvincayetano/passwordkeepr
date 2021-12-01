7982684772c789f3a089c0492a9a0405ed26d2a0
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-12-01 10:21:13 -0500

    🔥 Chore: seed a new organization with creator_id 1

--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -11,3 +11,8 @@ INSERT INTO category (name, description) VALUES
 ('Social', 'Social Media (eg. Facebook, Twitter, Instagram, etc)'),
 ('Work', 'Work Related'),
 ('Entertainment', 'Entertainment (eg. Youtube, Netflix, etc)');
+
+INSERT INTO organizations(name, description, creator_id) VALUES
+('Lighthouse Labs Student', 'Organization for students of lighthouse labs', 1);
+
+UPDATE users SET organization_id=1 WHERE id=1;
